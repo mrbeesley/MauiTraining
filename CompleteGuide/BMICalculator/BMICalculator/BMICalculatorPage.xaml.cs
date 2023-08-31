a3e158ac48e863e4ed2a42a54f295d0511927a19
Michael Beesley <mrbeesley@users.noreply.github.com>
2023-08-31 10:40:34 -0500

    bmi calculator training updates

--- CompleteGuide/BMICalculator/BMICalculator/BMICalculatorPage.xaml.cs
+++ CompleteGuide/BMICalculator/BMICalculator/BMICalculatorPage.xaml.cs
@@ -6,4 +6,25 @@ public partial class BMICalculatorPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void TapMale_Tapped(object sender, TappedEventArgs e)
+    {
+        FrameMale.BorderColor = Color.FromArgb("#0a0e19");
+        FrameFemale.BorderColor = Color.FromArgb("#fdfdfd");
+    }
+
+    private void TapFemale_Tapped(object sender, TappedEventArgs e)
+    {
+        FrameFemale.BorderColor = Color.FromArgb("#0a0e19");
+        FrameMale.BorderColor = Color.FromArgb("#fdfdfd");
+    }
+
+    private void BtnCalculateBMI_Clicked(object sender, EventArgs e)
+    {
+        var height = Convert.ToDouble(LblHeight.Text);
+        var weight = Convert.ToDouble(LblWeight.Text);
+
+        var bmi = (weight / height / height) * 10000;
+        DisplayAlert("Your BMI is:", bmi.ToString(), "OK");
+    }
 }
